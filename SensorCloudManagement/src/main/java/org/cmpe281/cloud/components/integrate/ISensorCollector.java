aea4e99f41166aef4edf7102fdf69ba7031ae283
Nakul Sharma <nakulsharma06@gmail.com>
2016-04-15 14:58:36 -0700

    Components interface updated

--- SensorCloudManagement/src/main/java/org/cmpe281/cloud/components/integrate/ISensorCollector.java
+++ SensorCloudManagement/src/main/java/org/cmpe281/cloud/components/integrate/ISensorCollector.java
@@ -1,11 +1,10 @@
 package org.cmpe281.cloud.components.integrate;
 
-import java.util.HashMap;
-
 import org.cmpe281.cloud.model.BarometerSensor;
 
-public interface ISensorCollector {
-	public HashMap<String, BarometerSensor>  getSensorData(String latlong); //fetching data
-	public boolean delete(String latlong); //stop observing
+import java.util.List;
 
+public interface ISensorCollector {
+	public List<BarometerSensor> getSensorData(String coordinate); //fetching data
+	public boolean delete(String coordinate); //stop observing
 }
